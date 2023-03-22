a712372a292c02354a7a0164c5116c8e8872592d
kw6rcw9 <katyadk2004@gmail.com>
2023-03-22 14:41:30 +0300

    ///

---
+++ Labs/Assets/Scripts/MovingPoints.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPoints : MonoBehaviour
+{
+    [SerializeField] Transform[] _points;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
